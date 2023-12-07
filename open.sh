a5b434ce1f254374ee4f6f69f1ad51f732445615
Alex Calderwood <alexander.d.calderwood@gmail.com>
2023-12-07 11:45:39 -0800

    Fixed lowercase in realization bug.

---
+++ open.sh
@@ -0,0 +1,5 @@
+URL="https://localhost:8101/html/instrument_study_1.html"
+# Firefox seems to work
+echo $URL
+open $URL
+
--- run.sh
+++ run.sh
@@ -1,2 +1,4 @@
-open http://[::]:8101/html/instrument_study_1.html
-python -m http.server 8101 
+python server.py 
+
+open https://localhost:8101/html/instrument_study_1.html
+
